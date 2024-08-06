859e63174ec2afd3997f0fb4e9c2c781f19be0ef
Varphone Wong <varphone@qq.com>
2024-08-06 15:43:49 +0800

    Refactor x86 and x64 builds

--- build.cmd
+++ build.cmd
@@ -2,6 +2,13 @@
 setlocal enabledelayedexpansion
 
 set PATH=%CD%\depot_tools;%PATH%
+set TARGET_CPU=%1
+
+echo "TARGET_CPU=%TARGET_CPU%"
+
+if "%TARGET_CPU%" == "" ( set TARGET_CPU=x64)
+
+echo "TARGET_CPU=%TARGET_CPU%"
 
 rem *** check dependencies ***
 
@@ -69,7 +76,7 @@ pushd angle.src
 
 set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 call gclient sync || exit /b 1
-call gn gen out/Release --args="target_cpu=x86 angle_build_all=false is_debug=false angle_has_frame_capture=false angle_enable_gl=false angle_enable_vulkan=false angle_enable_d3d9=false angle_enable_null=false" || exit /b 1
+call gn gen out/Release --args="target_cpu=\"%TARGET_CPU%\" angle_build_all=false is_debug=false angle_has_frame_capture=false angle_enable_gl=false angle_enable_vulkan=false angle_enable_d3d9=false angle_enable_null=false" || exit /b 1
 call git apply -p0 ..\angle.patch || exit /b 1
 call autoninja -C out/Release libEGL libGLESv2 libGLESv1_CM || exit /b 1
 popd
@@ -83,7 +90,7 @@ mkdir angle\include
 
 copy /y angle.src\.git\refs\heads\main angle\commit.txt 1>nul 2>nul
 
-copy /y "%ProgramFiles(x86)%\Windows Kits\10\Redist\D3D\x64\d3dcompiler_47.dll" angle\bin 1>nul 2>nul
+copy /y "%ProgramFiles(x86)%\Windows Kits\10\Redist\D3D\%TARGET_CPU%\d3dcompiler_47.dll" angle\bin 1>nul 2>nul
 
 copy /y angle.src\out\Release\libEGL.dll       angle\bin        1>nul 2>nul
 copy /y angle.src\out\Release\libGLESv1_CM.dll angle\bin        1>nul 2>nul
@@ -111,7 +118,7 @@ if "%GITHUB_WORKFLOW%" neq "" (
   :dateok
   set BUILD_DATE=%LDATE:~0,4%-%LDATE:~4,2%-%LDATE:~6,2%
 
-  %SZIP% a -mx=9 angle-%BUILD_DATE%.zip angle || exit /b 1
+  %SZIP% a -mx=9 angle-%TARGET_CPU%-%BUILD_DATE%.zip angle || exit /b 1
 
   echo ::set-output name=ANGLE_COMMIT::%ANGLE_COMMIT%
   echo ::set-output name=BUILD_DATE::%BUILD_DATE%
